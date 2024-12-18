819342152853ab62d644a76bf58f2947e04c2556
Igor Zhorkin <zhorkinigor@yandex.ru>
2024-12-18 11:35:38 +0300

    init commit

---
+++ frontend/src/components/Counter.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const counter = ref<number>(0);
+</script>
+
+<template>
+  <button
+    @click="counter++"
+    class="bg-blue-500 text-white px-3 py-1 rounded-md"
+  >
+    click me! {{ counter }}
+  </button>
+</template>
---
+++ frontend/src/pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Counter from "@/components/Counter.vue";
+</script>
+
+<template>
+  <main
+    class="flex flex-col justify-center items-center gap-8 h-screen pb-[20vh]"
+  >
+    <div class="flex items-start gap-3">
+      <h1 class="text-4xl text-stone-600">
+        Vue + Vite + Tailwind + Typescript
+      </h1>
+      <span class="text-stone-500 text-lg">template</span>
+    </div>
+    <Counter />
+  </main>
+</template>
